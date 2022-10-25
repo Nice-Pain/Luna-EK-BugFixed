9de7dd34f697b9a1490885c824eefa7730a3c45b
Nice-Pain <107368503+Nice-Pain@users.noreply.github.com>
2022-10-25 12:25:56 +0100

    Supports Big Screens

--- source/android/FlxHitbox.hx
+++ source/android/FlxHitbox.hx
@@ -28,6 +28,8 @@ class FlxHitbox extends FlxSpriteGroup {
     public var orgType:HitboxType = FOUR;
     public var orgAlpha:Float = 0.75;
     public var orgAntialiasing:Bool = true;
+
+    public var KEYS:Int = 4;
     
     public function new(type:HitboxType = FOUR, ?alphaAlt:Float = 0.75, ?antialiasingAlt:Bool = true)
     {
@@ -58,109 +60,123 @@ class FlxHitbox extends FlxSpriteGroup {
         {
             case ONE:
                 hitbox_hint.loadGraphic(Paths.image('androidcontrols/hitbox/1k_hint'));
+                KEYS = 1;
 
                 hitbox.add(add(k1 = createhitbox(0, 0, "k1")));
             case TWO:
                 hitbox_hint.loadGraphic(Paths.image('androidcontrols/hitbox/2k_hint'));
+                KEYS = 2;
 
                 hitbox.add(add(k1 = createhitbox(0, 0, "k1")));
-                hitbox.add(add(k2 = createhitbox(640, 0, "k2")));
+                hitbox.add(add(k2 = createhitbox(FlxG.width / 2, 0, "k2")));
             case THREE:
                 hitbox_hint.loadGraphic(Paths.image('androidcontrols/hitbox/3k_hint'));
+                KEYS = 3;
 
                 hitbox.add(add(k1 = createhitbox(0, 0, "k1")));
-                hitbox.add(add(k2 = createhitbox(426, 0, "k2")));
-                hitbox.add(add(k3 = createhitbox(852, 0, "k3")));
+                hitbox.add(add(k2 = createhitbox(FlxG.width / 3, 0, "k2")));
+                hitbox.add(add(k3 = createhitbox((FlxG.width / 3) * 2, 0, "k3")));
             case FOUR:
                 hitbox_hint.loadGraphic(Paths.image('androidcontrols/hitbox/4k_hint'));
+                KEYS = 4;
 
                 hitbox.add(add(k1 = createhitbox(0, 0, "k1")));
-                hitbox.add(add(k2 = createhitbox(320, 0, "k2")));
-                hitbox.add(add(k3 = createhitbox(640, 0, "k3")));
-                hitbox.add(add(k4 = createhitbox(960, 0, "k4")));
+                hitbox.add(add(k2 = createhitbox(FlxG.width / 4, 0, "k2")));
+                hitbox.add(add(k3 = createhitbox(FlxG.width / 2, 0, "k3")));
+                hitbox.add(add(k4 = createhitbox((FlxG.width / 4) * 3, 0, "k4")));
             case FIVE:
                 hitbox_hint.loadGraphic(Paths.image('androidcontrols/hitbox/5k_hint'));
+                KEYS = 5;
 
                 hitbox.add(add(k1 = createhitbox(0, 0, "k1")));
-                hitbox.add(add(k2 = createhitbox(256, 0, "k2")));
-                hitbox.add(add(k3 = createhitbox(512, 0, "k3")));
-                hitbox.add(add(k4 = createhitbox(768, 0, "k4")));
-                hitbox.add(add(k5 = createhitbox(1024, 0, "k5"))); 
+                hitbox.add(add(k2 = createhitbox(FlxG.width / 5, 0, "k2")));
+                hitbox.add(add(k3 = createhitbox((FlxG.width / 5) * 2, 0, "k3")));
+                hitbox.add(add(k4 = createhitbox((FlxG.width / 5) * 3, 0, "k4")));
+                hitbox.add(add(k5 = createhitbox((FlxG.width / 5) * 4, 0, "k5"))); 
             case SIX:
                 hitbox_hint.loadGraphic(Paths.image('androidcontrols/hitbox/6k_hint'));
+                KEYS = 6;
 
                 hitbox.add(add(k1 = createhitbox(0, 0, "k1")));
-                hitbox.add(add(k2 = createhitbox(213, 0, "k2")));
-                hitbox.add(add(k3 = createhitbox(426, 0, "k3")));
-                hitbox.add(add(k4 = createhitbox(639, 0, "k4")));
-                hitbox.add(add(k5 = createhitbox(852, 0, "k5")));
-                hitbox.add(add(k6 = createhitbox(1065, 0, "k6")));
+                hitbox.add(add(k2 = createhitbox(FlxG.width / 6, 0, "k2")));
+                hitbox.add(add(k3 = createhitbox(FlxG.width / 3, 0, "k3")));
+                hitbox.add(add(k4 = createhitbox((FlxG.width / 6) * 3, 0, "k4")));
+                hitbox.add(add(k5 = createhitbox((FlxG.width / 6) * 4, 0, "k5")));
+                hitbox.add(add(k6 = createhitbox((FlxG.width / 6) * 5, 0, "k6")));
             case SEVEN:
                 hitbox_hint.loadGraphic(Paths.image('androidcontrols/hitbox/7k_hint'));
+                KEYS = 7;
 
                 hitbox.add(add(k1 = createhitbox(0, 0, "k1")));
-                hitbox.add(add(k2 = createhitbox(182, 0, "k2")));
-                hitbox.add(add(k3 = createhitbox(364, 0, "k3")));
-                hitbox.add(add(k4 = createhitbox(546, 0, "k4")));
-                hitbox.add(add(k5 = createhitbox(728, 0, "k5")));
-                hitbox.add(add(k6 = createhitbox(910, 0, "k6")));
-                hitbox.add(add(k7 = createhitbox(1092, 0, "k7")));
+                hitbox.add(add(k2 = createhitbox(FlxG.width / 7, 0, "k2")));
+                hitbox.add(add(k3 = createhitbox((FlxG.width / 7) * 2, 0, "k3")));
+                hitbox.add(add(k4 = createhitbox((FlxG.width / 7) * 3, 0, "k4")));
+                hitbox.add(add(k5 = createhitbox((FlxG.width / 7) * 4, 0, "k5")));
+                hitbox.add(add(k6 = createhitbox((FlxG.width / 7) * 5, 0, "k6")));
+                hitbox.add(add(k7 = createhitbox((FlxG.width / 7) * 6, 0, "k7")));
             case EIGHT:
                 hitbox_hint.loadGraphic(Paths.image('androidcontrols/hitbox/8k_hint'));
+                KEYS = 8;
 
                 hitbox.add(add(k1 = createhitbox(0, 0, "k1")));
-                hitbox.add(add(k2 = createhitbox(160, 0, "k2")));
-                hitbox.add(add(k3 = createhitbox(320, 0, "k3")));
-                hitbox.add(add(k4 = createhitbox(480, 0, "k4")));
-                hitbox.add(add(k5 = createhitbox(640, 0, "k5")));
-                hitbox.add(add(k6 = createhitbox(800, 0, "k6")));
-                hitbox.add(add(k7 = createhitbox(960, 0, "k7")));
-                hitbox.add(add(k8 = createhitbox(1120, 0, "k8")));
+                hitbox.add(add(k2 = createhitbox(FlxG.width / 8, 0, "k2")));
+                hitbox.add(add(k3 = createhitbox(FlxG.width / 4, 0, "k3")));
+                hitbox.add(add(k4 = createhitbox((FlxG.width / 8) * 3, 0, "k4")));
+                hitbox.add(add(k5 = createhitbox(FlxG.width / 2, 0, "k5")));
+                hitbox.add(add(k6 = createhitbox((FlxG.width / 8) * 5, 0, "k6")));
+                hitbox.add(add(k7 = createhitbox((FlxG.width / 8) * 6, 0, "k7")));
+                hitbox.add(add(k8 = createhitbox((FlxG.width / 8) * 7, 0, "k8")));
             case NINE:
                 hitbox_hint.loadGraphic(Paths.image('androidcontrols/hitbox/9k_hint'));
+                KEYS = 9;
 
                 hitbox.add(add(k1 = createhitbox(0, 0, "k1")));
-                hitbox.add(add(k2 = createhitbox(142, 0, "k2")));
-                hitbox.add(add(k3 = createhitbox(284, 0, "k3")));
-                hitbox.add(add(k4 = createhitbox(426, 0, "k4")));
-                hitbox.add(add(k5 = createhitbox(568, 0, "k5")));
-                hitbox.add(add(k6 = createhitbox(710, 0, "k6")));
-                hitbox.add(add(k7 = createhitbox(852, 0, "k7")));
-                hitbox.add(add(k8 = createhitbox(994, 0, "k8")));
-                hitbox.add(add(k9 = createhitbox(1136, 0, "k9")));
+                hitbox.add(add(k2 = createhitbox(FlxG.width / 9, 0, "k2")));
+                hitbox.add(add(k3 = createhitbox((FlxG.width / 9) * 2, 0, "k3")));
+                hitbox.add(add(k4 = createhitbox((FlxG.width / 9) * 3, 0, "k4")));
+                hitbox.add(add(k5 = createhitbox((FlxG.width / 9) * 4, 0, "k5")));
+                hitbox.add(add(k6 = createhitbox((FlxG.width / 9) * 5, 0, "k6")));
+                hitbox.add(add(k7 = createhitbox((FlxG.width / 9) * 6, 0, "k7")));
+                hitbox.add(add(k8 = createhitbox((FlxG.width / 9) * 7, 0, "k8")));
+                hitbox.add(add(k9 = createhitbox((FlxG.width / 9) * 8, 0, "k9")));
             case TEN:
                 hitbox_hint.loadGraphic(Paths.image('androidcontrols/hitbox/10k_hint'));
+                KEYS = 10;
 
                 hitbox.add(add(k1 = createhitbox(0, 0, "k1")));
-                hitbox.add(add(k2 = createhitbox(128, 0, "k2")));
-                hitbox.add(add(k3 = createhitbox(256, 0, "k3")));
-                hitbox.add(add(k4 = createhitbox(384, 0, "k4")));    
-                hitbox.add(add(k5 = createhitbox(512, 0, "k5")));
-                hitbox.add(add(k6 = createhitbox(640, 0, "k6"))); 
-                hitbox.add(add(k7 = createhitbox(768, 0, "k7")));
-                hitbox.add(add(k8 = createhitbox(896, 0, "k8"))); 
-                hitbox.add(add(k9 = createhitbox(1024, 0, "k9")));
-                hitbox.add(add(k10 = createhitbox(1152, 0, "k10")));
+                hitbox.add(add(k2 = createhitbox(FlxG.width / 10, 0, "k2")));
+                hitbox.add(add(k3 = createhitbox(FlxG.width / 5, 0, "k3")));
+                hitbox.add(add(k4 = createhitbox((FlxG.width / 10) * 3, 0, "k4")));    
+                hitbox.add(add(k5 = createhitbox((FlxG.width / 10) * 4, 0, "k5")));
+                hitbox.add(add(k6 = createhitbox(FlxG.width / 2, 0, "k6"))); 
+                hitbox.add(add(k7 = createhitbox((FlxG.width / 10) * 6, 0, "k7")));
+                hitbox.add(add(k8 = createhitbox((FlxG.width / 10) * 7, 0, "k8"))); 
+                hitbox.add(add(k9 = createhitbox((FlxG.width / 10) * 8, 0, "k9")));
+                hitbox.add(add(k10 = createhitbox((FlxG.width / 10) * 9, 0, "k10")));
             case ELEVEN:
                 hitbox_hint.loadGraphic(Paths.image('androidcontrols/hitbox/11k_hint'));
+                KEYS = 11;
 
                 hitbox.add(add(k1 = createhitbox(0, 0, "k1")));
-                hitbox.add(add(k2 = createhitbox(116, 0, "k2")));
-                hitbox.add(add(k3 = createhitbox(232, 0, "k3")));
-                hitbox.add(add(k4 = createhitbox(348, 0, "k4")));    
-                hitbox.add(add(k5 = createhitbox(464, 0, "k5")));
-                hitbox.add(add(k6 = createhitbox(580, 0, "k6"))); 
-                hitbox.add(add(k7 = createhitbox(696, 0, "k7")));
-                hitbox.add(add(k8 = createhitbox(812, 0, "k8"))); 
-                hitbox.add(add(k9 = createhitbox(928, 0, "k9")));
-                hitbox.add(add(k10 = createhitbox(1044, 0, "k10"))); 
-                hitbox.add(add(k11 = createhitbox(1160, 0, "k11"))); 
+                hitbox.add(add(k2 = createhitbox(FlxG.width / 11, 0, "k2")));
+                hitbox.add(add(k3 = createhitbox((FlxG.width / 11) * 2, 0, "k3")));
+                hitbox.add(add(k4 = createhitbox((FlxG.width / 11) * 3, 0, "k4")));    
+                hitbox.add(add(k5 = createhitbox((FlxG.width / 11) * 4, 0, "k5")));
+                hitbox.add(add(k6 = createhitbox((FlxG.width / 11) * 5, 0, "k6"))); 
+                hitbox.add(add(k7 = createhitbox((FlxG.width / 11) * 6, 0, "k7")));
+                hitbox.add(add(k8 = createhitbox((FlxG.width / 11) * 7, 0, "k8"))); 
+                hitbox.add(add(k9 = createhitbox((FlxG.width / 11) * 8, 0, "k9")));
+                hitbox.add(add(k10 = createhitbox((FlxG.width / 11) * 9, 0, "k10"))); 
+                hitbox.add(add(k11 = createhitbox((FlxG.width / 11) * 10, 0, "k11"))); 
         }
 
         array = [k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11];
 
 	hitbox_hint.antialiasing = orgAntialiasing;
 	hitbox_hint.alpha = orgAlpha;
+        hitbox_hint.setGraphicSize(FlxG.width, FlxG.height);
+        hitbox_hint.updateHitbox();
+        hitbox_hint.x = 0;
 	add(hitbox_hint);
     }
 
@@ -168,6 +184,8 @@ class FlxHitbox extends FlxSpriteGroup {
 	var button = new FlxButton(x, y);
 	button.loadGraphic(FlxGraphic.fromFrame(getFrames().getByName(frames)));
 	button.antialiasing = orgAntialiasing;
+        button.setGraphicSize(Std.int(FlxG.width / KEYS), FlxG.height);
+        button.updateHitbox();
 	button.alpha = 0;// sorry but I can't hard lock the hitbox alpha
 	button.onDown.callback = function (){FlxTween.num(0, 0.75, 0.075, {ease:FlxEase.circInOut}, function(alpha:Float){ button.alpha = alpha;});};
 	button.onUp.callback = function (){FlxTween.num(0.75, 0, 0.1, {ease:FlxEase.circInOut}, function(alpha:Float){ button.alpha = alpha;});}
