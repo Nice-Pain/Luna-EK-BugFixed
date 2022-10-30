9d603fc19733ea31ad357cb4d99cea9ba8683b85
Nice-Pain <107368503+Nice-Pain@users.noreply.github.com>
2022-10-30 19:05:07 +0000

    Update FlxVirtualPad.hx

--- source/android/FlxVirtualPad.hx
+++ source/android/FlxVirtualPad.hx
@@ -6,6 +6,7 @@ import flixel.graphics.frames.FlxTileFrames;
 import flixel.group.FlxSpriteGroup;
 import flixel.math.FlxPoint;
 import flixel.util.FlxDestroyUtil;
+import flixel.ui.FlxButton;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.graphics.FlxGraphic;
 import openfl.utils.Assets;
